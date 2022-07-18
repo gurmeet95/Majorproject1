b6163580ebaac83ee9387a34c95f9e2b3f582a6a
gurmeet95 <cgurmeet8@gmail.com>
2022-07-18 11:50:49 +0530

    ejs and routers/controllers

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.posting = function(req, res) {
+    return res.end('<h1>User Posts</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const homecontroller = require('../controllers/home_controller')
 console.log('Router loaded');
-router.get('/', homecontroller.home)
-
+router.get('/', homecontroller.home);
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+// for any further routes,access from here
+//router.use()'/routername',require('terfile);.rou
 module.exports = router;
