e63a4fd777d718cb386ee1b80a943f318b91a5ea
gurmeet95 <cgurmeet8@gmail.com>
2022-07-25 11:03:42 +0530

    SetUp Home View

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
  //use expres router
  app.use('/', require('./routes'));
  // set up view engine
- app.set('/view engine', 'ejs');
- app.set('views', '/views');
+ app.set('view engine', 'ejs');
+ app.set('views', './views');
 
  app.listen(port, function(err) {
      if (err) {
