6078c5f15954a4db100dd4352b7f3f391bf2621c
gurmeet95 <cgurmeet8@gmail.com>
2022-08-08 11:33:37 +0530

    Connection to Mongoose And Static file Layout

--- index.js
+++ index.js
@@ -2,7 +2,12 @@
  const app = express();
  const port = 8000;
  const expressLayouts=require('express-ejs-layouts');
+ const db=require('./config/mongoose');
+ app.use(express.static('./assets'));
  app.use(expressLayouts);
+ //exctract style and scripts from sub pages into layouts
+ app.set('layout extractStyles',true);
+ app.set('layout extractScripts',true);
  //use expres router
  app.use('/', require('./routes'));
  // set up view engine
