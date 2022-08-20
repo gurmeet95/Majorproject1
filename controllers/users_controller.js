4e562f6dc7d6eb1af312113f23998391a79f8f4d
gurmeet95 <cgurmeet8@gmail.com>
2022-08-20 14:04:53 +0530

    Manual Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,3 +4,23 @@ module.exports.profile = function(req, res) {
         
     });
 }
+// render the signup page.
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+}
+// render the sign in page.
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    })
+}
+// get the signup data
+module.exports.create=function(req,res){
+    //to do
+}
+// signin and create hte session for user.
+module.exports.createsession=function(req,res){
+    //to do
+}
