789a5b5f8d09e17fc6b21fc602f6fd03e63fe56c
gurmeet95 <cgurmeet8@gmail.com>
2022-09-02 13:41:33 +0530

    Passport Authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,4 @@
-const User = require('../models/user1');
+const User = require('../models/user');
 
 
 module.exports.profile = function(req, res) {
@@ -9,18 +9,25 @@ module.exports.profile = function(req, res) {
 }
 // render the signup page.
 module.exports.signUp=function(req,res){
+    if(req.isAuthenticated()){
+      return   res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title:"Codeial | Sign Up"
     })
 }
 // render the sign in page.
 module.exports.signIn=function(req,res){
+    if(req.isAuthenticated()){
+       return  res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title:"Codeial | Sign In"
     })
 }
 // get the signup data
 module.exports.create=function(req,res){
+    
     //to do
     if(req.body.password != req.body.Confirm_password){
         return res.redirect('back');
@@ -44,5 +51,14 @@ module.exports.create=function(req,res){
 }
 // signin and create hte session for user.
 module.exports.createsession=function(req,res){
-    //to do
+    return res.redirect('/');
+}
+module.exports.destroySession=function(req,res){
+    req.logout(function(err){
+        if(err){
+            return next(err);   //new way on which show in video.update version
+        }
+    }); 
+
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -4,6 +4,11 @@
  const port = 8000;
  const expressLayouts=require('express-ejs-layouts');
  const db=require('./config/mongoose');
+ //user for session cookie
+ const session=require('express-session');
+ const passport=require('passport');
+ const passportLocal= require('./config/passportlocalstrategy');
+ const MongoStore  = require('connect-mongo');   //new way found from stackoverflow
  app.use(express.urlencoded());
  app.use(cookieParser());
  app.use(express.static('./assets'));
@@ -11,11 +16,36 @@
  //exctract style and scripts from sub pages into layouts
  app.set('layout extractStyles',true);
  app.set('layout extractScripts',true);
- //use expres router
- app.use('/', require('./routes'));
+
  // set up view engine
  app.set('view engine', 'ejs');
  app.set('views', './views');
+ app.use(session({
+    name:'codieal',
+    //Todo Change the seret before deploymnet in projeft mode
+    secret: 'blahsomething',
+    resave: false,
+    cookie:{
+        maxAge:(1000 *60 * 100)
+    },
+    store:MongoStore.create({
+        mongoUrl:'mongodb://localhost/codeial_devlopment',
+        autoRemove:'disabled'
+    },
+    function(err){
+        console.log(err || 'Connect-mongo')
+
+    }
+    )
+  
+
+
+ }));
+ app.use(passport.initialize());
+ app.use(passport.session());
+ app.use(passport.setAuthenticatedUser);
+  //use expres router
+  app.use('/', require('./routes'));
 
  app.listen(port, function(err) {
      if (err) {
