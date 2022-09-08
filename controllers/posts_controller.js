c7d42942187ce39c92743107155bee5abf9f304f
gurmeet95 <cgurmeet8@gmail.com>
2022-09-08 12:25:20 +0530

    Posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,3 +1,13 @@
-module.exports.posting = function(req, res) {
-    return res.end('<h1>User Posts</h1>');
+const Post=require('../models/post')
+
+module.exports.create=function(req,res){
+    Post.create({
+        content: req.body.content,
+        user: req.user._id
+    },function(err,post){
+        if(err){
+            console.log('Eror in creating a post'); return;}
+        return res.redirect('back');
+    });
+
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const postsController = require('../controllers/posts_controller');
-router.get('/posting', postsController.posting);
-module.exports = router;
+const postsController=require('../controllers/posts_controller');
+router.post('/create',postsController.create);
+
+module.exports=router;
