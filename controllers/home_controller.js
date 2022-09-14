ff012ab2d882ca3d91586bc269f29f2019a8b992
gurmeet95 <cgurmeet8@gmail.com>
2022-09-14 16:52:16 +0530

    update name email and do some styling in home css

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const User=require('../models/user')
 
 module.exports.home = function(req, res){
     // console.log(req.cookies);
@@ -21,10 +22,13 @@ module.exports.home = function(req, res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
+      User.find({},function(er,users){
+            return res.render('home', {
             title: "Codeial | Home",
-            posts:  posts
+            posts:  posts,
+            all_users:users
         });
+    });
     })
 
 }
