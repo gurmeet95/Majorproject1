1e063a82ab9e8cd2f991116ee497ac0715430143
gurmeet95 <cgurmeet8@gmail.com>
2022-09-20 09:44:41 +0530

    converting to ajax

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,10 +2,20 @@ const Post = require('../models/post');
 const Comment=require('../models/comment')
 
 try{module.exports.create = async function(req, res){
-    await Post.create({
+  let post=   await Post.create({
          content: req.body.content,
          user: req.user._id
      });
+
+     if(req.xhr){
+        return res.status(200).json({
+            data:{
+                post: post
+            },
+            message: 'Post Ceated'
+
+        });
+     }
      req.flash('success','Post Published');
       return res.redirect('back');
  }
@@ -25,7 +35,16 @@ try{module.exports.create = async function(req, res){
                if(post.user == req.user.id){
                  post.remove();
               await  Comment.deleteMany({post: req.params.id});
+              if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id:req.params.id
+                    },
+                    messagae: "Post deleted"
+                })
+              }
               req.flash('success','Post deleted');
+
                      return res.redirect('back');
                  
              }else{
